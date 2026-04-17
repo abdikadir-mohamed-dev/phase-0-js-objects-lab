9ac419008e2c95071a15389fe844dff07e5cd942
abdikadir-mohamed-dev <abdikadirmohammed180@gmail.com>
2026-04-17 11:48:37 +0300

    objects-lab

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 //Write your code here
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: 'VIP',
+  ticketPrice: 150.00
+};
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+function updateTicketType(attendee, newType) {
+  attendee.ticketType = newType;
+}
+
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 
 
-//Needed for the tests to work. Don't modify
+
+//Needed for the tests to work. Don't modify this.
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
